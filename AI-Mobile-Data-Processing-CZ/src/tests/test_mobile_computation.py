2e17c793e4a7b49fd86c8b892d8a417983eda03f
SoLowMow <110025570+solowmow@users.noreply.github.com>
2023-11-18 00:37:13 +0100

    Update test_mobile_computation.py

--- AI-Mobile-Data-Processing-CZ/src/tests/test_mobile_computation.py
+++ AI-Mobile-Data-Processing-CZ/src/tests/test_mobile_computation.py
@@ -1,1 +1,35 @@
+import unittest
+from mobile_computation.mobile_server.server_code import process_data
+from mobile_computation.mobile_app.android.app_code import AndroidApp
+from mobile_computation.mobile_app.ios.app_code import iOSApp
+
+class TestMobileComputation(unittest.TestCase):
+    def setUp(self):
+        # Initialize test data or resources if needed
+        pass
+
+    def tearDown(self):
+        # Clean up after each test if necessary
+        pass
+
+    def test_process_data(self):
+        # Test data processing functionality in the server
+        data = [1, 2, 3, 4, 5]  # Example input data
+        processed_data = process_data(data)
+        self.assertEqual(len(processed_data), len(data))  # Assert processing maintains data length
+
+    def test_android_app(self):
+        # Test Android app functionality
+        android_app = AndroidApp()
+        result = android_app.run()
+        self.assertTrue(result)  # Assert that the app runs successfully
+
+    def test_ios_app(self):
+        # Test iOS app functionality
+        ios_app = iOSApp()
+        result = ios_app.run()
+        self.assertTrue(result)  # Assert that the app runs successfully
+
+if __name__ == '__main__':
+    unittest.main()
  
